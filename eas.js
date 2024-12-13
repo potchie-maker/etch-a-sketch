0e56b468e4591647ecc507dc3f65dc10bbaa7948
potchie-maker <177373729+potchie-maker@users.noreply.github.com>
2024-12-13 01:02:31 -0500

    Make function that creates basic grid layout

---
+++ eas.js
@@ -0,0 +1,19 @@
+const container = document.querySelector("#container");
+
+function createGrid(num) {
+  let containerSize = 500;
+  container.style.width = `${containerSize}px`;
+  container.style.height = `${containerSize}px`;
+
+  let squareSize = containerSize/num;
+
+  for (let i = 1; i <= (num * num); i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
+    container.appendChild(square);
+  }
+}
+
+createGrid(2);
